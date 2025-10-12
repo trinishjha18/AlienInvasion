8bfe696a3af420c90c564f131ac8ba12431752de
Trinish Jha <trinishjha@gmail.com>
2025-10-12 10:22:52 -0400

    Add alien_invasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,21 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
