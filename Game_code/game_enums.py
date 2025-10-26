1c1275e921d4fb6aba46483536d670c06a93a758
Rahul Tevatia <rtevatia2@gmail.com>
2025-10-26 15:10:48 -0400

    Refactor alien_invasion

---
+++ Game_code/game_enums.py
@@ -0,0 +1,12 @@
+from enum import Enum, IntEnum
+
+class Colors(Enum):
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    
+class GameConfig(IntEnum):
+    SCREEN_WIDTH = 1280
+    SCREEN_HEIGHT = 720
+    IMAGE_WIDTH = 100
+    IMAGE_HEIGHT = 100
+    FPS = 60
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,45 +1,47 @@
 import pygame
+from Game_code.game_enums import *
 
-# TODO: Declare constant variables
-SCREEN_WIDTH = 1280
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((GameConfig.SCREEN_WIDTH.value, GameConfig.SCREEN_HEIGHT.value))
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-IMAGE_WIDTH = 100
-
-
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, 720))
-clock = pygame.time.Clock()
-running = True
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Load image on the screen
-    # FIXME: Find background free files for this project.
-    image = pygame.image.load("Images/Spaceship5.png")
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.render_image()
+            # flip() the display to put your work on screen
+            pygame.display.flip()
+            self.clock.tick(GameConfig.FPS.value)
+            
+    def handle_events(self):
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
     
-    # Resize any image
-    image = pygame.transform.scale(image, (IMAGE_WIDTH, 100))
-    
-    # Get coordinates of image
-    image_rect = image.get_rect()
-    screen_rect = screen.get_rect()
-    
-    # Align image and screen together
-    image_rect.bottom = screen_rect.bottom
-    image_rect.centerx = screen_rect.centerx
+    def render_image(self):
+        # Load image on the screen
+            # FIXME: Find background free files for this project.
+            image = pygame.image.load("Images/Spaceship5.png")
+            
+            # Resize any image
+            image = pygame.transform.scale(image, (GameConfig.IMAGE_WIDTH.value, GameConfig.IMAGE_HEIGHT.value))
+            
+            # Get coordinates of image
+            image_rect = image.get_rect()
+            screen_rect = self.screen.get_rect()
+            
+            # Align image and screen together
+            image_rect.bottom = screen_rect.bottom
+            image_rect.centerx = screen_rect.centerx
 
-    screen.blit(image, image_rect)
+            # Display image on the screen
+            self.screen.blit(image, image_rect)
 
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+if __name__ == "__main__":
+    game = AlienInvasion()
+    game.run()
+    pygame.quit()
